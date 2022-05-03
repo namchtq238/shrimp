d42a30234f361e0cd27d06efca41342ce45187a3
namchtq238 <kaizuto123@gmail.com>
2022-05-04 00:20:39 +0700

    inital

---
+++ src/main/java/com/pondmanage/PondManageApplication.java
@@ -0,0 +1,13 @@
+package com.pondmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PondManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PondManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pondmanage/PondManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pondmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PondManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
